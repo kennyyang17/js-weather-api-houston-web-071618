69b423338f90dc8ee95ed80901f4559585b09b96
Kenny Yang <kennyyang19@gmail.com>
2018-08-30 14:43:40 -0500

    done

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,9 +1,18 @@
 function getFahrenheit(result){
   // Your code goes here
+  return result.map(function (hour){
+    return hour.temperature
+  })
 }
 
 function getHour(result){
   // Your code goes here
+  return result.map(function (hour){
+    let myDate = new Date(hour.time * 1000)
+    return myDate.getHours()
+  })
+  // let myDate = new Date(time * 1000)
+  // return myDate.getHours()
 }
 
 function generateDataSet(labels, data) {
@@ -28,7 +37,13 @@ function generateDataSet(labels, data) {
 
 function makeRequest(endpoint, canvas) {
   // Your code goes here
-
+  fetch(endpoint)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJSON) {
+    let hourlyData = myJSON.hourly.data;
+      const tempChart = new Chart(canvas, generateDataSet(getHour(hourlyData), getFahrenheit(hourlyData)))
+  });
   // After your fetch works - use your json data with the line below :)
-  // const tempChart = new Chart(canvas, generateDataSet(getHour(hourlyData), getFahrenheit(hourlyData)))
 }
